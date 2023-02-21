35553034a84cc756ddda69053e6b63128e912904
Jean François <jf.jullien@digireferent.fr>
2023-02-21 12:05:53 +0100

    ajout d'un commentaire

--- categorize-values.py
+++ categorize-values.py
@@ -2,7 +2,7 @@ myMixedTypeList = [45, 290578, 1.02, True, "my dog is on the bed", "45"]
 for item in myMixedTypeList :
     print("{} is of the data type {}".format(item,type(item)))
 
-
+// je suis un commentaire
 
 
 
